28d49e22bbff7a214a424975e377c6c0c4b29884
Omotayo <kehindeomotayo11@gmail.com>
2023-08-09 13:30:14 +0100

    Done With This

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+function saturdayFun(msg = "roller-skate") {
+  return `This Saturday, I want to ${msg}!`;
+}
+console.log(saturdayFun("bathe my dog"));
+
+function mondayWork(msg = "go to the office") {
+  return `This Monday, I will ${msg}.`;
+}
+
+// function wrapAdjective() {
+//   const msg = function (adjective) {
+//     return `You are ${adjective}`;
+//   };
+//   return msg("good");
+// }
+// wrapAdjective();
+
+function wrapAdjective(symbol) {
+  return function (adjective) {
+    return `You are ${symbol}${adjective}${symbol}!`;
+  };
+}
+
+const wrapWithAsterisk = wrapAdjective("*");
+const wrapWithHash = wrapAdjective("#");
+
+console.log(wrapWithAsterisk("awesome"));
+console.log(wrapWithHash("fantastic"));
